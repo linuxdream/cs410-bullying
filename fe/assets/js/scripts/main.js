c5b8ec9ed4adfe15a55596c7c322ffbf76723182
Brandon Ching <bdching@gmail.com>
2016-11-13 14:33:48 -0700

    Progress on FB parsing. Still need to pull comments and enable next pages.

--- fe/assets/js/scripts/main.js
+++ fe/assets/js/scripts/main.js
@@ -18,8 +18,8 @@ $(document).ready(function () {
 
         $('#loginbutton,#feedbutton').removeAttr('disabled');
 
-        FB.getLoginStatus(function fbLoginCheck(response){
-            if(response.status === 'connected'){
+        FB.getLoginStatus(function fbLoginCheck(response) {
+            if (response.status === 'connected') {
                 new PNotify({
                     title: 'Success',
                     text: 'Successfully connected to Facebook!',
@@ -31,9 +31,9 @@ $(document).ready(function () {
 
                 //Enable the serch bar
                 enableSearchBar();
-            }else{
-                FB.login(function fbLogin(response){
-                    if(response.status === 'connected'){
+            } else {
+                FB.login(function fbLogin(response) {
+                    if (response.status === 'connected') {
                         new PNotify({
                             title: 'Success',
                             text: 'Successfully connected to Facebook!',
@@ -44,74 +44,95 @@ $(document).ready(function () {
                         sessionStorage.setItem('fbAccount', JSON.stringify(response));
 
                         enableSearchBar();
-                    }else{
+                    } else {
                         new PNotify({
                             title: 'Error',
                             text: 'Could not connect to Facebook.',
                             type: 'error'
                         });
                     }
-                }, {scope: ['email', 'user_friends']});
+                }, {
+                    scope: ['email', 'user_posts']
+                });
             }
         });
     });
 
     //Bind the search bar to the FB search
     $('#fbaccount').autocomplete({
-        source: function(request, response){
-            searchFBAccounts(request.term, function(data){
-                console.log('fb seasrch data', data);
-                response(data);
-            });
-        },
-        minLength: 3,
-        select: function(event, ui){
-            $('.selected-account').empty();
-            $('.selected-account').append('<img src="' + ui.item.image + '"><br>' + ui.item.name);
-            $('#fbaccount').val(ui.item.name + ' - ' + ui.item.id);
+            source: function (request, response) {
+                searchFBAccounts(request.term, function (data) {
+                    console.log('fb seasrch data', data);
+                    response(data);
+                });
+            },
+            minLength: 3,
+            select: function (event, ui) {
+                $('.selected-account').empty();
+                $('.selected-account').append('<img src="' + ui.item.image + '"><br>' + ui.item.name);
+                $('#fbaccount').val(ui.item.name + ' - ' + ui.item.id);
 
-            return false;
-        }
-    })
-    .autocomplete("instance")
-    ._renderItem = function(ul, item){
-        return $('<li>')
-            .append('<img src="' + item.image + '">' + item.name)
-            .appendTo(ul);
-    };
+                return false;
+            }
+        })
+        .autocomplete("instance")
+        ._renderItem = function (ul, item) {
+            return $('<li>')
+                .append('<img src="' + item.image + '">' + item.name)
+                .appendTo(ul);
+        };
 
-    $('#analyze').on('click', function(){
+    $('#analyze').on('click', function () {
         //Get the account info
         var fbid = $('#fbaccount').val().split(' - ')[1];
+        var requestFields = [
+            'id',
+            'created_time',
+            'description',
+            'from',
+            'link',
+            'message',
+            'name',
+            'story',
+            'to',
+            'type',
+            'comments.summary(1)',
+            'likes.summary(1)'
+        ];
+        var params = {
+            since: moment(moment().subtract(50, 'months'), 'YYYY-MM-DD hh:mm A').unix(),
+            until: moment(moment(), 'YYYY-MM-DD hh:mm A').unix(),
+            limit: 25,
+            fields: requestFields.join(',')
+        };
 
         //Make the first request
-        FB.api('/' + fbid + '/', 'get', params, function(response){
-            if(response.error){
+        FB.api('/' + fbid + '/posts', 'get', params, function (response) {
+            document.processingNotification = new PNotify({
+                title: 'Parsing',
+                text: 'Please wait while we process your request...',
+                type: 'warning',
+                hide: false
+            });
+
+            if (response.error) {
                 return cb(response.error.message);
             }
 
-            if(response.data && response.data.length){
+            if (response.data && response.data.length) {
                 account.allResultsLength = account.allResultsLength + response.data.length;
 
-                //Make API request to save
-                ApiRequest.makeRequest('POST', 'parsedata', {data: response.data, parserID: $scope.parser.id, postEndpoint: account.endpoint, source: $scope.parser.source})
-                    .success(function(res){
-                        account.notify.update({text: 'Retrieved and saved '+response.data.length+' posts for a total of '+account.allResultsLength+'. Checking for more results...'});
-                    })
-                    .error(function(res){
-                        return cb('There was a problem with the query parameters and no posts were found.');
-                    });
 
                 //Check for more pages..calls cb()
-                getNextPage(response, account);
-            }else{
+                //getNextPage(response, account);
+            } else {
                 console.log(response);
 
                 return cb('There was a problem with the query parameters and no posts were found.');
             }
 
-            function getNextPage(response, account){
-                if(response && response.paging && response.paging.next){
+            function getNextPage(response, account) {
+                if (response && response.paging && response.paging.next) {
                     $http.get(response.paging.next)
                         .then(function (res) {
                             //Save prev page data
@@ -119,12 +140,19 @@ $(document).ready(function () {
                                 account.allResultsLength = account.allResultsLength + res.data.data.length;
 
                                 //Make API request to save
-                                ApiRequest.makeRequest('POST', 'parsedata', {data: res.data.data, parserID: $scope.parser.id, postEndpoint: account.endpoint, source: $scope.parser.source})
-                                    .success(function(d){
-                                        account.notify.update({text: 'Retrieved and saved '+res.data.data.length+' posts for a total of '+account.allResultsLength+'. Checking for more results...'});
+                                ApiRequest.makeRequest('POST', 'parsedata', {
+                                        data: res.data.data,
+                                        parserID: $scope.parser.id,
+                                        postEndpoint: account.endpoint,
+                                        source: $scope.parser.source
+                                    })
+                                    .success(function (d) {
+                                        account.notify.update({
+                                            text: 'Retrieved and saved ' + res.data.data.length + ' posts for a total of ' + account.allResultsLength + '. Checking for more results...'
+                                        });
                                         getNextPage(res.data, account);
                                     })
-                                    .error(function(d){
+                                    .error(function (d) {
                                         console.log('fail', res);
                                     });
                             } else {
@@ -132,7 +160,7 @@ $(document).ready(function () {
                                 return cb('No data found in subsequent pages.');
                             }
                         });
-                }else{
+                } else {
                     //All done
                     cb();
                 }
@@ -145,19 +173,19 @@ $(document).ready(function () {
     /**
      * Helper functions
      */
-    function enableSearchBar(){
+    function enableSearchBar() {
         $('#fbaccount').attr('disabled', false);
     }
 
-    function searchFBAccounts(searchString, cb){
-        if(searchString && searchString.length > 2){
+    function searchFBAccounts(searchString, cb) {
+        if (searchString && searchString.length > 2) {
             FB.api('/search/', {
                 type: 'user',
                 limit: 30,
                 fields: 'id,name,picture,link',
                 q: searchString
             }, function (response) {
-                if(_.has(response, 'error')){
+                if (_.has(response, 'error')) {
                     new PNotify({
                         title: 'Error',
                         text: 'Cannot search Facebook using that query',
@@ -165,17 +193,21 @@ $(document).ready(function () {
                     });
                     return [];
                 }
-console.log('yoy', response);
+
                 var cleanResponses = [];
 
                 _.each(response.data, function (r) {
                     // cleanResponses.push({id: r, text: r.name + ', ' + location + ' - ' + r.link})
-                    cleanResponses.push({image: r.picture.data.url, name: r.name, id: r.id})
+                    cleanResponses.push({
+                        image: r.picture.data.url,
+                        name: r.name,
+                        id: r.id
+                    })
                 });
 
                 cb(cleanResponses);
             });
-        }else{
+        } else {
             return cb([]);
         }
     }
